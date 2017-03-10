f3a33b669cf153d7bb250cf6866222894a64c6b3
TarikHuber <huber.tarik@gmail.com>
2017-03-10 19:21:54 +0100

    Refactored localisation

--- src/Auth/Authentication.php
+++ src/Auth/Authentication.php
@@ -10,13 +10,15 @@ class Authentication {
 	public static function attempt($request, $User, $settings){
 
 		$api_key=$request->getHeaderLine($settings['auth_header']['api_key_name']);
-		$locale=$request->getHeaderLine($settings['auth_header']['locale']?:'locale');
+		$locale=$request->getHeaderLine('locale');
 
 		if (strpos($api_key, 'Bearer ') !== false) {
 			$api_key=str_replace('Bearer ','',$api_key);
 			self::$current_user=$User->where('api_key', $api_key)->first();
 
-			l::setLocale($locale);
+			if(strlen($locale)>0){
+				l::setLocale($locale);
+			}
 
 			return self::$current_user;
 		}
